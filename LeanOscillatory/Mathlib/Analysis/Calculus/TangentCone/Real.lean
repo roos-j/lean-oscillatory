f93af986f58949bc3ae5120ff9f8b54bcd9a870f
Joris Roos <roos.jor@gmail.com>
2026-06-29 08:14:12 +0200

    update

--- LeanOscillatory/Mathlib/Analysis/Calculus/TangentCone/Real.lean
+++ LeanOscillatory/Mathlib/Analysis/Calculus/TangentCone/Real.lean
@@ -11,14 +11,14 @@ open scoped Topology NNReal
 
 section Real
 
-theorem uniqueDiffOn_uIcc {a b : ℝ} (hab : a ≠ b) : UniqueDiffOn ℝ (uIcc a b) :=
-  uniqueDiffOn_Icc <| min_lt_max.mpr hab
+-- theorem uniqueDiffOn_uIcc {a b : ℝ} (hab : a ≠ b) : UniqueDiffOn ℝ (uIcc a b) :=
+--   uniqueDiffOn_Icc <| min_lt_max.mpr hab
 
--- maybe not needed
-theorem uniqueDiffOn_uIoo (a b : ℝ) : UniqueDiffOn ℝ (uIoo a b) := uniqueDiffOn_Ioo _ _
+-- -- maybe not needed
+-- theorem uniqueDiffOn_uIoo (a b : ℝ) : UniqueDiffOn ℝ (uIoo a b) := uniqueDiffOn_Ioo _ _
 
--- maybe not needed
-theorem uniqueDiffOn_uIoc (a b : ℝ) : UniqueDiffOn ℝ (uIoc a b) := uniqueDiffOn_Ioc _ _
+-- -- maybe not needed
+-- theorem uniqueDiffOn_uIoc (a b : ℝ) : UniqueDiffOn ℝ (uIoc a b) := uniqueDiffOn_Ioc _ _
 
 end Real
 
